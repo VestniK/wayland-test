4d8e4e832d56fd157c07bd1dd3a04ba9ea4e62f7
Sergey Vidyuk <sir.vestnik@gmail.com>
2018-05-16 19:55:06 +0700

    Use log4cplus and fmt for logging

--- main.cpp
+++ main.cpp
@@ -1,6 +1,14 @@
 #include <array>
 #include <iostream>
 
+#include <log4cplus/logger.h>
+#include <log4cplus/loggingmacros.h>
+#include <log4cplus/configurator.h>
+#include <log4cplus/initializer.h>
+
+#include <fmt/format.h>
+#include <fmt/ostream.h>
+
 #include <portable_concurrency/future>
 
 #include "io/file.hpp"
@@ -12,6 +20,7 @@
 #include "xkb.hpp"
 
 using namespace std::literals;
+using namespace fmt::literals;
 
 wl::keycode keycode_conv(xkb::keycode val) noexcept {
   return wl::keycode{ut::underlying_cast(val) - 8};
@@ -23,7 +32,7 @@ xkb::keycode keycode_conv(wl::keycode val) noexcept {
 
 struct kb_handler {
   void keymap(wl::keyboard::ref, wl::keyboard::keymap_format fmt, int fd, size_t size) try {
-    std::cout << "Received keymap. Format: " << fmt << "; of size: " << size << '\n';
+    LOG4CPLUS_DEBUG(log, "Received keymap. Format: {}; of size: {}"_format(fmt, size));
     if (fmt != wl::keyboard::keymap_format::xkb_v1)
       throw std::runtime_error{"Unsupported keymap format"};
 
@@ -34,21 +43,21 @@ struct kb_handler {
     if (esc_xkb == xkb::keycode::invalid)
       throw std::runtime_error{"failed to find Escape key code"};
     esc_keycode = keycode_conv(esc_xkb);
-    std::cout << "Escape keycode: " << ut::underlying_cast(esc_keycode) << '\n';
+    LOG4CPLUS_DEBUG(log, "Escape keycode: {}"_format(ut::underlying_cast(esc_keycode)));
   } catch(...) {
     promise.set_exception(std::current_exception());
   }
 
   void enter(wl::keyboard::ref, wl::serial eid, wl::surface::ref, wl_array*) {
-    std::cout << "Surface get focus[" << eid << "]\n";
+    LOG4CPLUS_DEBUG(log, "Surface get focus[{}]"_format(eid));
   }
   void leave(wl::keyboard::ref, wl::serial eid, wl::surface::ref) {
-    std::cout << "Surface lost focus[" << eid << "]\n";
+    LOG4CPLUS_DEBUG(log, "Surface lost focus[{}]"_format(eid));
   }
   void key(wl::keyboard::ref, wl::serial eid, wl::clock::time_point time, wl::keycode key, wl::keyboard::key_state state) {
-    std::cout
-      << "Key event[" << eid << "] timestamp: " << time.time_since_epoch().count() << "; key code: " << ut::underlying_cast(key)
-      << " [" << kmap.key_get_name(keycode_conv(key)) << "] " << "; key state: " << state << '\n';
+    LOG4CPLUS_DEBUG(log, "Key event[{}] timestamp: {}; key code: {} [{}]; key state: {}"_format(
+      eid, time.time_since_epoch().count(), ut::underlying_cast(key), kmap.key_get_name(keycode_conv(key)), state
+    ));
 
     if (key == esc_keycode && state == wl::keyboard::key_state::pressed) {
       promise.set_value();
@@ -59,14 +68,15 @@ struct kb_handler {
     wl::keyboard::ref, wl::serial eid, uint32_t mods_depressed, uint32_t mods_latched,
     uint32_t mods_locked, uint32_t group
   ) {
-    std::cout
-      << "Modifiers event[" << eid << "] mds_depresed: " << mods_depressed << "; mods latched: " << mods_latched
-      << "; mods locked: " << mods_locked << "; group: " << group << '\n';
+    LOG4CPLUS_DEBUG(log, "Modifiers event[{}] mds_depresed: {}; mods latched: {}; mods locked: {}; group: {}"_format(
+      eid, mods_depressed, mods_latched, mods_locked, group
+    ));
   }
   void repeat_info(wl::keyboard::ref, int32_t rate, std::chrono::milliseconds delay) {
-    std::cout << "Repeat info: rate: " << rate << "; delay: " << delay.count() << "ms\n";
+    LOG4CPLUS_DEBUG(log, "Repeat info: rate: {}; delay: {}ms"_format(rate, delay.count()));
   }
 
+  log4cplus::Logger log;
   pc::promise<void> promise;
   xkb::keymap kmap;
   wl::keycode esc_keycode = {};
@@ -74,15 +84,22 @@ struct kb_handler {
 
 pc::future<void> wait_quit(wl::seat& seat) {
   std::cout << "==== Press Esc to quit ====\n";
+  log4cplus::Logger log = log4cplus::Logger::getInstance("INPUT");
+
+  LOG4CPLUS_DEBUG(log, "Seat version: {}"_format(seat.get_version()));
   struct {
     void capabilities(wl::seat::ref, ut::bitmask<wl::seat::capability> caps) {
-      std::cout << "\tSeat capabilities: " << caps.value() << '\n';
+      LOG4CPLUS_DEBUG(log, "Seat capabilities: {}"_format(caps.value()));
     }
-    void name(wl::seat::ref, const char* seat_name) {std::cout << "\tSeat name: " << seat_name << '\n';}
-  } seat_logger;
+    void name(wl::seat::ref, const char* seat_name) {
+      LOG4CPLUS_DEBUG(log, "Seat name: {}"_format(seat_name));
+    }
+
+    log4cplus::Logger log;
+  } seat_logger{log};
   seat.add_listener(seat_logger);
 
-  kb_handler kb_listener;
+  kb_handler kb_listener{seat_logger.log};
   wl::keyboard kb = seat.get_keyboard();
   kb.add_listener(kb_listener);
 
@@ -90,43 +107,44 @@ pc::future<void> wait_quit(wl::seat& seat) {
 }
 
 pc::future<int> start(wl::display& display, wl::compositor compositor, wl::shell shell, wl::shm shm, wl::seat seat) {
-  std::cout << "Compositor version: " << compositor.get_version() << '\n';
-  std::cout << "Shell version: " << shell.get_version() << '\n';
-  std::cout << "SHM version: " << shm.get_version() << '\n';
-  std::cout << "Seat version: " << seat.get_version() << '\n';
+  auto log = log4cplus::Logger::getInstance("UI");
+
+  LOG4CPLUS_DEBUG(log, "Compositor version: {}"_format(compositor.get_version()));
+  LOG4CPLUS_DEBUG(log, "Shell version: {}"_format(shell.get_version()));
+  LOG4CPLUS_DEBUG(log, "SHM version: {}"_format(shm.get_version()));
 
   wl::surface surface = compositor.create_surface();
-  std::cout << "Created a surface of version: " << surface.get_version() << '\n';
+  LOG4CPLUS_DEBUG(log, "Created a surface of version: {}"_format(surface.get_version()));
   wl::shell_surface shsurf = shell.get_shell_surface(surface);
-  std::cout << "Created a shell surface of version: " << shsurf.get_version() << '\n';
+  LOG4CPLUS_DEBUG(log, "Created a shell surface of version: {}"_format(shsurf.get_version()));
   shsurf.set_toplevel();
 
-  auto shm_listener = [](wl::shm::ref, wl::shm::format fmt) {
-    const auto flags = std::cout.flags();
-    std::cout << "\tsupported pixel format code: " << std::hex << static_cast<uint32_t>(fmt) << '\n';
-    std::cout.setf(flags);
+  auto shm_listener = [log](wl::shm::ref, wl::shm::format fmt) {
+    LOG4CPLUS_DEBUG(log, "supported pixel format code: {:x}"_format(ut::underlying_cast(fmt)));
   };
   shm.add_listener(shm_listener);
 
   struct {
     void ping(wl::shell_surface::ref surf, wl::serial serial) {
-      std::cout << "ping: " << serial << '\n';
+      LOG4CPLUS_DEBUG(log, "ping: {}"_format(serial));
       surf.pong(serial);
     }
     void configure(wl::shell_surface::ref, uint32_t edges, wl::size sz) {
-      std::cout << "\tshell_surface cofiguration: edges = " << edges << "; size = " << sz.width << 'x' << sz.height << '\n';
+      LOG4CPLUS_DEBUG(log, "shell_surface cofiguration: edges: {}; size: {}x{}"_format(edges, sz.width, sz.height));
     }
     void popup_done(wl::shell_surface::ref) {
-      std::cout << "popup done\n";
+      LOG4CPLUS_DEBUG(log, "popup done");
     }
-  } shell_surface_logger;
+
+    log4cplus::Logger log;
+  } shell_surface_logger{log};
   shsurf.add_listener(shell_surface_logger);
 
   io::shared_memory shmem{4*240*480};
   wl::shm::pool pool = shm.create_pool(shmem.fd().native_handle(), shmem.size());
-  std::cout << "wl_shm_pool version: " << pool.get_version() << '\n';
+  LOG4CPLUS_DEBUG(log, "Created a wl::shm::pool of version: {}"_format(pool.get_version()));
   wl::buffer buf = pool.create_buffer(0, wl::size{240, 480}, 4*240, wl::shm::format::ARGB8888);
-  std::cout << "wl_buffer version: " << buf.get_version() << '\n';
+  LOG4CPLUS_DEBUG(log, "Created a wl::buffer of version: {}"_format(buf.get_version()));
   std::fill(shmem.data(), shmem.data() + shmem.size(), std::byte{0x88});
   surface.attach(buf);
   surface.commit();
@@ -143,6 +161,10 @@ pc::future<int> start(wl::display& display, wl::compositor compositor, wl::shell
 }
 
 int main(int argc, char** argv) try {
+  log4cplus::Initializer log_init;
+  log4cplus::BasicConfigurator log_conf;
+  log_conf.configure();
+
   wl::display display{argc > 1 ? argv[1] : nullptr};
 
   registry_searcher<wl::compositor, wl::shell, wl::shm, wl::seat> searcher;
