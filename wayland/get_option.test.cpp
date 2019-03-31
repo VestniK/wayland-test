bda5885fb7bdbf6045d917266779885ec0f7114c
Sergey Vidyuk <sir.vestnik@gmail.com>
2019-04-01 00:15:30 +0700

    Switch to catch

--- wayland/get_option.test.cpp
+++ wayland/get_option.test.cpp
@@ -1,18 +1,34 @@
-#include <gtest/gtest.h>
+#define CATCH_CONFIG_MAIN
+#include <catch2/catch.hpp>
+
+#include <string_view>
+#include <vector>
 
 #include <wayland/get_option.hpp>
 
 namespace test {
 
-struct get_option : ::testing::Test {};
+using namespace std::literals;
 
-TEST_F(get_option, returns_nullptr_on_option_absence) {
+TEST_CASE("get_option") {
   char arg0[] = "prog";
   char arg1[] = "-d";
   char arg2[] = "wayland-1";
-  char* argv[] = {arg0, arg1, arg2};
-  int argc = 3;
-  EXPECT_EQ(::get_option(argc, argv, "-s"), nullptr);
+  std::vector<char*> args = {arg0, arg1, arg2};
+  char** argv = args.data();
+  int argc = static_cast<int>(args.size());
+
+  SECTION("returns nullptr when there is no requested option") {
+    REQUIRE(get_option(argc, argv, "-s") == nullptr);
+  }
+  SECTION("returns requested option value") {
+    REQUIRE(get_option(argc, argv, "-d") == "wayland-1"sv);
+  }
+  SECTION("removes found option") {
+    get_option(argc, argv, "-d");
+    REQUIRE(argc == 1);
+    REQUIRE(argv[0] == "prog"sv);
+  }
 }
 
 } // namespace test
