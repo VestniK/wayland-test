6d6777ece7c119d70c853aac54944a152299a9a1
Sergey Vidyuk <sir.vestnik@gmail.com>
2019-04-21 00:31:12 +0700

    Cube

--- wayland/gles2/renderer.cpp
+++ wayland/gles2/renderer.cpp
@@ -52,21 +52,68 @@ renderer::renderer()
     )")},
       program_{link(vertex_shader_, fragment_shader_)}, ibo_{gen_buffer()},
       vbo_{gen_buffer()} {
-  static const vertex vertices[] = {{{-1., 1., 0.}, {0., 0., -1.}},
-      {{1., 1., 0.}, {0., 0., -1.}}, {{-1., -1., 0.}, {0., 0., -1.}},
-      {{1., -1., 0.}, {0., 0., -1.}}};
+  // clang-format off
+  static const vertex vertices[] = {
+    {{-1., 1., -1.}, {0., 0., -1.}},
+    {{1., 1., -1.}, {0., 0., -1.}},
+    {{-1., -1., -1.}, {0., 0., -1.}},
+    {{1., -1., -1.}, {0., 0., -1.}},
+
+    {{1., 1., -1.}, {1., 0., 0.}},
+    {{1., 1., 1.}, {1., 0., 0.}},
+    {{1., -1., -1.}, {1., 0., 0.}},
+    {{1., -1., 1.}, {1., 0., 0.}},
+
+    {{-1., 1., -1.}, {-1., 0., 0.}},
+    {{-1., 1., 1.}, {-1., 0., 0.}},
+    {{-1., -1., -1.}, {-1., 0., 0.}},
+    {{-1., -1., 1.}, {-1., 0., 0.}},
+
+    {{-1., 1., -1.}, {0., 1., 0.}},
+    {{1., 1., -1.}, {0., 1., 0.}},
+    {{1., 1., 1.}, {0., 1., 0.}},
+    {{-1., 1., 1.}, {0., 1., 0.}},
+
+    {{-1., -1., -1.}, {0., -1., 0.}},
+    {{1., -1., -1.}, {0., -1., 0.}},
+    {{1., -1., 1.}, {0., -1., 0.}},
+    {{-1., -1., 1.}, {0., -1., 0.}},
+
+    {{-1., 1., 1.}, {0., 0., 1.}},
+    {{1., 1., 1.}, {0., 0., 1.}},
+    {{-1., -1., 1.}, {0., 0., 1.}},
+    {{1., -1., 1.}, {0., 0., 1.}}
+  };
+  // clang-format on
   glBindBuffer(GL_ARRAY_BUFFER, vbo_.get());
   glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-  static const GLuint idxs[] = {0, 1, 2, 1, 3, 2};
+  // clang-format off
+  static const GLuint idxs[] = {
+    0, 1, 2, 1, 3, 2,
+
+    4, 5, 6, 6, 5, 7,
+
+    8, 9, 10, 10, 9, 11,
+
+    12, 13, 14, 14, 12, 15,
+
+    16, 17, 18, 18, 16, 19,
+
+    20, 21, 22, 21, 23, 22
+  };
+  // clang-format on
   glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo_.get());
   glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(idxs), idxs, GL_STATIC_DRAW);
 
   glUseProgram(program_.get());
+
+  glEnable(GL_DEPTH_TEST);
+  glDepthFunc(GL_LESS);
+  glClearColor(0, 0, 0, .9);
 }
 
 void renderer::resize(size sz) {
-  glClearColor(0, 0, 0, .9);
   sz_ = sz;
   const float len = std::min(sz.width, sz.height);
   glViewport(0, 0, sz.width, sz.height);
@@ -93,7 +140,7 @@ void renderer::draw(clock::time_point ts) {
   glm::vec3 light_pos = {
       3. * std::cos(spot_angle), 6. * std::sin(2.5 * spot_angle), 0.0};
   glm::mat4 model = glm::translate(glm::vec3{0, 0, 6}) *
-                    glm::rotate(glm::mat4{1.}, angle, {0, 1, 1});
+                    glm::rotate(glm::mat4{1.}, angle, {.5, .3, .1});
   glm::mat3 norm_rotation = glm::transpose(glm::inverse(glm::mat3(model)));
 
   const GLint norm_rotation_uniform =
@@ -105,7 +152,7 @@ void renderer::draw(clock::time_point ts) {
       glGetAttribLocation(program_.get(), "position");
   const GLint normal_location = glGetAttribLocation(program_.get(), "normal");
 
-  glClear(GL_COLOR_BUFFER_BIT);
+  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
   glUniformMatrix4fv(model_uniform, 1, GL_FALSE, glm::value_ptr(model));
   glUniformMatrix3fv(
@@ -120,6 +167,6 @@ void renderer::draw(clock::time_point ts) {
   glEnableVertexAttribArray(position_location);
   glEnableVertexAttribArray(normal_location);
   glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo_.get());
-  glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr);
+  glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_INT, nullptr);
   glFlush();
 }
