006cb192e69d6dbcb4e43507aa4b86d3902b050d
Sergey Vidyuk <sir.vestnik@gmail.com>
2021-03-11 00:41:41 +0700

    More fmtlib usages

--- wayland/main.cpp
+++ wayland/main.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+#include <fmt/format.h>
 
 #include <wayland/event_loop.hpp>
 #include <wayland/gles_window.hpp>
@@ -6,13 +6,14 @@
 #include <wayland/util/get_option.hpp>
 #include <wayland/util/xdg.hpp>
 
-int main(int argc, char** argv) {
+int main(int argc, char **argv) {
   if (get_flag(argc, argv, "-h")) {
-    std::cout << "Ussage: " << argv[0] << " [-d DISPLAY] [-s SCRIPT_PATH]\n";
-    std::cout << "\t-d DISPLAY\tSpecify wayland display. Current session "
-                 "default is used if nowhing is specified.\n";
-    std::cout << "\t-s SCRIPT_PATH\tPath to the script to play. XDG config "
-                 "wayland.chai is searched if nothing specified\n";
+    fmt::print("Usage: {} [-d DISPLAY] [-s SCRIPT_PATH]\n"
+               "\t-d DISPLAY\tSpecify wayland display. Current session default "
+               "is used if nowhing is specified.\n"
+               "\t-s SCRIPT_PATH\tPath to the script to play. XDG config "
+               "wayland.chai is searched if nothing specified\n",
+               argv[0]);
     return EXIT_SUCCESS;
   }
 
@@ -23,8 +24,8 @@ int main(int argc, char** argv) {
   while (!wnd.is_closed() && !wnd.is_initialized())
     eloop.dispatch();
 
-  play_script(
-      get_option(argc, argv, "-s", xdg::find_config("wayland.chai")), wnd);
+  play_script(get_option(argc, argv, "-s", xdg::find_config("wayland.chai")),
+              wnd);
 
   return EXIT_SUCCESS;
 }
