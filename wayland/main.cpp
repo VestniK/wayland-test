b8b2edb1264a9a444e308c39960e83887bdeb5c1
Sergey Vidyuk <sir.vestnik@gmail.com>
2021-08-12 01:50:51 +0700

    Play with external io event loop

--- wayland/main.cpp
+++ wayland/main.cpp
@@ -1,10 +1,27 @@
 #include <fmt/format.h>
 
+#include <asio/awaitable.hpp>
+#include <asio/io_service.hpp>
+#include <asio/posix/stream_descriptor.hpp>
+#include <asio/use_awaitable.hpp>
+
 #include <wayland/event_loop.hpp>
 #include <wayland/gles_window.hpp>
 #include <wayland/script_player.hpp>
 #include <wayland/util/get_option.hpp>
 #include <wayland/util/xdg.hpp>
+
+template <typename Exec, typename Pred>
+asio::awaitable<void> async_wait(const Exec &exec, event_loop &eloop,
+                                 Pred &&pred) {
+  asio::posix::stream_descriptor conn{exec,
+                                      wl_display_get_fd(&eloop.get_display())};
+  while (!pred()) {
+    co_await conn.async_wait(asio::posix::stream_descriptor::wait_read,
+                             asio::use_awaitable);
+    eloop.dispatch_pending();
+  }
+}
 
 int main(int argc, char **argv) {
   if (get_flag(argc, argv, "-h")) {
@@ -17,11 +34,15 @@ int main(int argc, char **argv) {
     return EXIT_SUCCESS;
   }
 
+  asio::io_service io;
   event_loop eloop{get_option(argc, argv, "-d")};
   gles_window wnd{eloop, 1337};
 
-  while (!wnd.is_initialized())
-    eloop.dispatch();
+  {
+    auto task = async_wait(io.get_executor(), eloop,
+                           [&wnd] { return wnd.is_initialized(); });
+    io.run();
+  }
 
   play_script(get_option(argc, argv, "-s", xdg::find_config("wayland.chai")),
               wnd);
