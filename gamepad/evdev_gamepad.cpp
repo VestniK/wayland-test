9086ebbe85a6d633629b9bc60300de88d90ae009
Sergey Vidyuk <sir.vestnik@gmail.com>
2023-04-13 23:06:14 +0700

    Initial events read code

--- gamepad/evdev_gamepad.cpp
+++ gamepad/evdev_gamepad.cpp
@@ -7,9 +7,19 @@
 #include <asio/detached.hpp>
 #include <asio/posix/stream_descriptor.hpp>
 
+#include <fmt/chrono.h>
 #include <spdlog/spdlog.h>
 
 #include <util/io.hpp>
+
+namespace {
+
+template <size_t Extent = std::dynamic_extent>
+auto span2buf(std::span<std::byte, Extent> in) {
+  return asio::buffer(in.data(), in.size());
+}
+
+} // namespace
 
 evdev_gamepad::evdev_gamepad(asio::io_context::executor_type io_executor,
                              const std::filesystem::path &devnode) {
@@ -37,6 +47,34 @@ evdev_gamepad::watch_device(asio::io_context::executor_type io_executor,
             namebuf.data()) == -1)
     throw std::system_error{errno, std::system_category(), "ioctl(EVIOCGNAME)"};
   spdlog::debug("gamepad name: {}", namebuf.data());
+
+  std::array<input_event, 32> events;
+  while (true) {
+    size_t bytes_read = 0;
+    while (bytes_read == 0 || bytes_read % sizeof(input_event) != 0) {
+      // TODO handle EOF properly
+      bytes_read += co_await conn.async_read_some(
+          span2buf(
+              std::as_writable_bytes(std::span{events}).subspan(bytes_read)),
+          asio::use_awaitable);
+    }
+    const std::span<const input_event> events_read =
+        std::span{events}.subspan(bytes_read / sizeof(input_event));
+    for (const auto &ev : events_read) {
+      switch (ev.type) {
+      case EV_KEY:
+        spdlog::debug("key even: time: {} code: {} value: {}",
+                      std::chrono::seconds{ev.time.tv_sec} +
+                          std::chrono::microseconds{ev.time.tv_usec},
+                      ev.code, ev.value);
+      case EV_ABS:
+        spdlog::debug("abs event: time: {} code: {} value: {}",
+                      std::chrono::seconds{ev.time.tv_sec} +
+                          std::chrono::microseconds{ev.time.tv_usec},
+                      ev.code, ev.value);
+      }
+    }
+  }
 
   co_return;
 }
