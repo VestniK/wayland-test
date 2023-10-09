f543e87473ab69d27c21c4ff0594a13b454c29de
Sergey Vidyuk <sir.vestnik@gmail.com>
2023-10-10 00:28:21 +0700

    Small cleanup and syling

--- scene/triangular_net.test.cpp
+++ scene/triangular_net.test.cpp
@@ -1,5 +1,3 @@
-#include <iostream>
-
 #include <catch2/catch_test_macros.hpp>
 #include <catch2/generators/catch_generators.hpp>
 #include <catch2/generators/catch_generators_adapters.hpp>
--- util/bitmask.hpp
+++ util/bitmask.hpp
@@ -1,36 +1,48 @@
 #pragma once
 
+#include <compare>
 #include <type_traits>
 
-template<typename E>
+template <typename E>
 class bitmask {
 public:
   using value_type = std::underlying_type_t<E>;
 
-  constexpr bitmask(const E e) noexcept: val(static_cast<value_type>(e)) {}
+  constexpr bitmask(const E e) noexcept : val(static_cast<value_type>(e)) {}
   constexpr bitmask() = default;
 
-  constexpr bitmask operator|(const bitmask rhs) const noexcept {return bitmask(val | rhs.val);}
-  bitmask& operator|=(const bitmask rhs) noexcept {val = static_cast<value_type>(val | rhs.val); return *this;}
+  constexpr bitmask operator|(const bitmask rhs) const noexcept {
+    return bitmask(val | rhs.val);
+  }
+  bitmask& operator|=(const bitmask rhs) noexcept {
+    val = static_cast<value_type>(val | rhs.val);
+    return *this;
+  }
 
-  constexpr bitmask operator&(const bitmask rhs) const noexcept {return bitmask(val & rhs.val);}
-  bitmask& operator&=(const bitmask rhs) noexcept {val = static_cast<value_type>(val & rhs.val); return *this;}
+  constexpr bitmask operator&(const bitmask rhs) const noexcept {
+    return bitmask(val & rhs.val);
+  }
+  bitmask& operator&=(const bitmask rhs) noexcept {
+    val = static_cast<value_type>(val & rhs.val);
+    return *this;
+  }
 
-  constexpr bitmask operator^(const bitmask rhs) const noexcept {return bitmask(val ^ rhs.val);}
-  bitmask& operator^=(const bitmask rhs) noexcept {val = static_cast<value_type>(val ^ rhs.val); return *this;}
+  constexpr bitmask operator^(const bitmask rhs) const noexcept {
+    return bitmask(val ^ rhs.val);
+  }
+  bitmask& operator^=(const bitmask rhs) noexcept {
+    val = static_cast<value_type>(val ^ rhs.val);
+    return *this;
+  }
 
-  constexpr bitmask operator~() const noexcept {return bitmask{~val};}
+  constexpr bitmask operator~() const noexcept { return bitmask{~val}; }
 
-  constexpr bool operator<(const bitmask rhs) const noexcept {return val < rhs.val;}
-  constexpr bool operator<=(const bitmask rhs) const noexcept {return val <= rhs.val;}
-  constexpr bool operator==(const bitmask rhs) const noexcept {return val == rhs.val;}
-  constexpr bool operator!=(const bitmask rhs) const noexcept {return val != rhs.val;}
-  constexpr bool operator>=(const bitmask rhs) const noexcept {return val >= rhs.val;}
-  constexpr bool operator>(const bitmask rhs) const noexcept {return val > rhs.val;}
+  constexpr std::strong_ordering operator<=>(
+      const bitmask&) const noexcept = default;
 
-  constexpr explicit operator bool() const noexcept {return val != 0;}
+  constexpr explicit operator bool() const noexcept { return val != 0; }
 
-  constexpr value_type value() const noexcept {return val;}
+  constexpr value_type value() const noexcept { return val; }
 
 private:
   constexpr bitmask(const value_type t) noexcept : val(t) {}
