b24a6e9e204ca3ba5a5d146427d7242334fd6f1c
Sergey Vidyuk <sir.vestnik@gmail.com>
2024-01-14 00:07:51 +0700

    Better target_sources cmake func signature

--- cmake/target_shaders.cmake
+++ cmake/target_shaders.cmake
@@ -1,12 +1,22 @@
-function(target_shaders Tgt Incdir)
-  foreach(Arg ${ARGN})
+function(target_shaders Tgt)
+  set(options "")
+  set(oneValueArgs INCLUDE_DIR)
+  set(multiValueArgs SOURCES)
+  cmake_parse_arguments(TGT_SHADERS
+    "${options}"
+    "${oneValueArgs}"
+    "${multiValueArgs}"
+    ${ARGN}
+)
+
+  foreach(Arg ${TGT_SHADERS_SOURCES})
     list(APPEND InputArgs "-i" ${Arg})
   endforeach()
 
   add_custom_command(
     OUTPUT ${CMAKE_CURRENT_BINARY_DIR}/shaders.cpp ${CMAKE_CURRENT_BINARY_DIR}/shaders.hpp
-    COMMAND tools::shaders2consts -o ${CMAKE_CURRENT_BINARY_DIR}/shaders.cpp --header ${CMAKE_CURRENT_BINARY_DIR}/shaders.hpp -d ${CMAKE_CURRENT_BINARY_DIR}/shaders.cpp.d -I ${Incdir} ${InputArgs}
-    DEPENDS ${ARGN} tools::shaders2consts
+    COMMAND tools::shaders2consts -o ${CMAKE_CURRENT_BINARY_DIR}/shaders.cpp --header ${CMAKE_CURRENT_BINARY_DIR}/shaders.hpp -d ${CMAKE_CURRENT_BINARY_DIR}/shaders.cpp.d -I ${TGT_SHADERS_INCLUDE_DIR} ${InputArgs}
+    DEPENDS ${TGT_SHADERS_SOURCES} tools::shaders2consts
     DEPFILE ${CMAKE_CURRENT_BINARY_DIR}/shaders.cpp.d
     WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
   )
@@ -16,6 +26,6 @@ function(target_shaders Tgt Incdir)
   target_sources(${Tgt} PRIVATE
     ${CMAKE_CURRENT_BINARY_DIR}/shaders.cpp
     ${CMAKE_CURRENT_BINARY_DIR}/shaders.hpp
-    ${ARGN}
+    ${TGT_SHADERS_SOURCES}
   )
 endfunction()
