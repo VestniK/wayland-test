b911a16cb8fa6f89cd7b26d4598980eb00e459e1
Sergey Vidyuk <sir.vestnik@gmail.com>
2025-01-14 22:24:41 +0700

    Update deps

--- conanfile.py
+++ conanfile.py
@@ -6,14 +6,14 @@ class Deps(ConanFile):
     generators = "CMakeDeps"
 
     def requirements(self):
-        self.requires("asio/1.31.0")
+        self.requires("asio/1.32.0")
         self.requires("fmt/11.0.1", override=True)
         self.requires("glm/cci.20230113")
         self.requires("libpng/1.6.44")
         self.requires("mp-units/2.3.0")
         self.requires("spdlog/1.14.1")
         self.requires("tracy/cci.20220130")
-        self.requires("vulkan-headers/1.3.290.0")
+        self.requires("vulkan-headers/1.3.296.0")
 
     def build_requirements(self):
         self.test_requires("catch2/3.7.1")
