c9f538c7f86fe6b8d71e2050177475199686999c
Sergey Vidyuk <sir.vestnik@gmail.com>
2025-11-29 01:32:52 +0700

    Atomically update mp-units, spdlog, fmt friends to the latest ver

--- conanfile.py
+++ conanfile.py
@@ -7,11 +7,11 @@ class Deps(ConanFile):
 
     def requirements(self):
         self.requires("asio/1.36.0")
-        self.requires("fmt/11.0.1", override=True)
+        self.requires("fmt/12.1.0", override=True)
         self.requires("glm/cci.20230113")
         self.requires("libpng/1.6.44")
-        self.requires("mp-units/2.3.0", options={'std_format': False})
-        self.requires("spdlog/1.14.1")
+        self.requires("mp-units/2.4.0", options={'std_format': False})
+        self.requires("spdlog/1.16.0")
         self.requires("tracy/cci.20220130")
         self.requires("vulkan-headers/1.3.296.0", override=True)
         self.requires("vulkan-memory-allocator/3.0.1")
